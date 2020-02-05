8f7e131c94a466d40964d53e053e44fbfff03f7b
David Corredor <vidmore8@gmail.com>
2020-02-05 23:21:26 +0000

    exercise 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,6 @@
 int main(void)
 {
 
-	const char* string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, string, strlen(string));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
