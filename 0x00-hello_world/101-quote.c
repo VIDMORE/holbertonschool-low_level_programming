03cae968883c5de8172cb996b96f50b8f474e34e
David Corredor <vidmore8@gmail.com>
2020-02-05 23:18:13 +0000

    exercise 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
-#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
 /**
-* main - wtites a text without using printf
+* main - wtites a text only using write
 * Return: 1
 **/
 
 int main(void)
 {
 
-	const char* string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	const char* string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, string, strlen(string));
 	return (1);
