d8ebf572d301b0bc94c4757ec42be703df1e0dae
David Corredor <vidmore8@gmail.com>
2020-02-05 23:02:08 +0000

    task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stlib.h>
+#include <string.h>
+
+/**
+* main - wtites a text without using printf
+* Return: 1
+**/
+
+int main(void)
+{
+
+	char string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(2, string, strlen(string);
+	return (1);
+}
