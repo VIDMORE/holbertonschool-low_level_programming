4579774452836e661015dd3cf272cd938b582b1f
David Corredor <vidmore8@gmail.com>
2020-02-06 18:36:42 +0000

    task #5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - function to print numbers from o to 9
+* Return: 0
+**/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
