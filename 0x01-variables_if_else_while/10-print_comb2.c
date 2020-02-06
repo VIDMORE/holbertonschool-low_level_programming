c989fe33036e498e4ff3c3dd14f74e7cd793c7d2
David Corredor <vidmore8@gmail.com>
2020-02-06 21:18:18 +0000

    task #10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - possible combinations of single digit numbers
+* Return: 0
+**/
+
+int main(void)
+{
+	int n;
+/*	int c; */
+	int v;
+
+	for (n = 48; n <= 57; n++)
+	{
+		
+		for (v = 48; v <= 57; v++)
+		{
+			putchar(n);
+			putchar(v);
+	
+			if (v != 57 || n != 57)
+			{
+			putchar(44);
+			putchar(32);
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
+
