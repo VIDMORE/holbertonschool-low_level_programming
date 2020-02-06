f33e6ee0c440879fba2a724583d0b5bdaf10a9ba
David Corredor <vidmore8@gmail.com>
2020-02-06 18:09:06 +0000

    fix task #2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+/**
+* main - function to print the hole alphabet
+* Return: 0
+**/
+
 int main(void)
 {
 	char c;
 
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 		if (c == 'z')
@@ -12,6 +17,5 @@ int main(void)
 			putchar('\n');
 		}
 	}
-	
 	return (0);
 }
