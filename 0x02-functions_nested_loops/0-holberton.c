3cd987c5267b7944971e5f7f7cca9971976e5ab6
David Corredor <vidmore8@gmail.com>
2020-02-10 21:50:44 +0000

    0x02 tasks

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+*
+*
+**/
+
+int main(void)
+{
+	char i[] = "Holberton";
+	int counter;
+	
+	for (counter = 0; counter <= 9 ; counter++)
+	{
+		_putchar(i[counter]);
+	}
+	_putchar('\n');
+	return (0);
+	
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+**In this repository you will find all the excersices for 0x02. C - Functions, nested loops**
