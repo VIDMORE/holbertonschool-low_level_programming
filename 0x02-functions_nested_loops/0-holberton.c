d6a4c3082e8e99141d63c51098ad8f2b99fa0a5d
David Corredor <vidmore8@gmail.com>
2020-02-11 18:04:28 +0000

    fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,19 @@
 #include "holberton.h"
 
 /**
-*
-*
+* main - Function to print using _putchar
+* Return: 0
 **/
 
 int main(void)
 {
 	char i[9] = "Holberton";
 	int counter;
-	
-	for (counter = 0; counter <= 9 ; counter++)
+
+	for (counter = 0; counter < 9 ; counter++)
 	{
 		_putchar(i[counter]);
 	}
 	_putchar('\n');
 	return (0);
-	
 }
