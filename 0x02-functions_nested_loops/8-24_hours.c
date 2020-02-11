74eb7fa255fe6024abd47329e018e85ebaa965d0
David Corredor <vidmore8@gmail.com>
2020-02-11 22:26:28 +0000

    number #8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+void jack_bauer(void)
+{
+	int h1;
+	int h2;
+
+	for (h1 = 0; h1 <= 23; h1++)
+	{
+		for (h2 = 0; h2 <= 59; h2++)
+		{
+
+			_putchar(h1 / 10 + '0');
+			_putchar(h1 % 10 + '0');
+			_putchar(':');
+			_putchar(h2 / 10 + '0');
+			_putchar(h2 % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
