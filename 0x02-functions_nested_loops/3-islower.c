627b7c7fc963570739ef624c9b6d88b20046bfbc
David Corredor <vidmore8@gmail.com>
2020-02-11 18:32:21 +0000

    fix #3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,19 @@
 #include "holberton.h"
 
+/**
+* _islower - function to identify lower characters
+* Return: 1 if true 0 if false
+* @c: - character
+**/
+
 int _islower(int c)
-{  
+{
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
 	else
 	{
- 		return (0);
+		return (0);
 	}
 }
