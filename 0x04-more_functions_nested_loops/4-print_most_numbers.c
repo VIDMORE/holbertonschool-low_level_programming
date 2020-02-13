e09ba82b854bb1c0ae4b34687a88f61769ac5136
David Corredor <vidmore8@gmail.com>
2020-02-13 18:37:48 +0000

    fix 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,15 @@
 #include "holberton.h"
 
+/**
+* print_most_numbers - print numbers 10 times
+* 
+**/
+
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = '0'; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		if (i != '2' && i != '4')
 		{
