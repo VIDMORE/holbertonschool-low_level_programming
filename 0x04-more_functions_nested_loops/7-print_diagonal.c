69a7cc526ca164d3f0a438994b2f204fc9de80f6
David Corredor <vidmore8@gmail.com>
2020-02-13 19:34:16 +0000

    fix 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,14 +12,13 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		for (a = 0; a < i; a+)
+		for (a = 0; a < i; a++)
 		{
 			_putchar(' ');
 		}
 		_putchar('\\');
 		_putchar('\n');
 	}
-
 	if (n <= 0)
 	{
 		_putchar('\n');
