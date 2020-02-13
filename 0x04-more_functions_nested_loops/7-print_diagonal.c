4a59b53cadee965e6e6ef3a3c2ce7e5794e598ee
David Corredor <vidmore8@gmail.com>
2020-02-13 19:31:10 +0000

    fix 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,31 +1,26 @@
 #include "holberton.h"
 
 /**
-* print_diagonal - print a back slash
-* @n: number of spaces
-**/
+ * print_diagonal - print a back slash
+ * @n: number of spaces
+ **/
 
 void print_diagonal(int n)
 {
 	int i;
 	int a;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i <= n; i++)
-		{
-			if (i != 0)
-			{
-				for (a = 0; a < i; a++)
-				{
-					_putchar(' ');
-				}
-				_putchar('\\');
-				_putchar('\n');
-			}
+		for (a = 0; a < i; a++)
+		{		
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
-	else
+
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
