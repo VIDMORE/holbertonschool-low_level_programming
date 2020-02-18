1c283092062ab9dc41e3ba98a150a16e2a670133
David Corredor <vidmore8@gmail.com>
2020-02-18 17:21:25 +0000

    task #7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* puts_half - prints half of a string
+* @str: string to split
+**/
+
+void puts_half(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (i /= 2; str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
