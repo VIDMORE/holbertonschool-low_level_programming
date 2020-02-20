a032ff5c0d0321cbd198fa7c1dc5aff76eaf7b10
David Corredor <vidmore8@gmail.com>
2020-02-20 19:31:12 +0000

    fix #2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,9 +10,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; j < n; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[j] = src[j];
+		if (j < n)
+		{
+			dest[j] = src[j];
+		}
 	}
+	dest[j] = '\0';
 	return (dest);
 }
