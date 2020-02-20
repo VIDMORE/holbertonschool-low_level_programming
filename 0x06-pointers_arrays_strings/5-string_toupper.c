af655630b39b03f7882dc202905208c4cc46d4f3
David Corredor <vidmore8@gmail.com>
2020-02-20 22:56:06 +0000

    task #5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+/**
+* string_toupper - changes all lowercase letters of a string to uppercase
+* @a: string
+* Return: array chandeg
+**/
+
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (a[i] >= 'a' || a[i] <= 'z')
+		{
+			a[i] = (a[i] - 32);
+		}
+	}
+	return (a);
+}
