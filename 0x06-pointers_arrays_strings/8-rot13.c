8190622d48c3ef4aa67566cb728b007cb5bbf1e0
David Corredor <vidmore8@gmail.com>
2020-02-21 02:37:52 +0000

    uploading task #8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+/**
+* rot13 - function that encodes a string using rot13
+* @a: string
+* Return: string changed
+**/
+
+char *rot13(char *a)
+{
+	int i, j;
+	char rot[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char tor[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (rot[j] == a[i])
+			{
+				a[i] = tor[j];
+			}
+		}
+	}
+	return (a);
+}
