e27e9473e645a60777afc3e2d71011efea426a56
David Corredor <vidmore8@gmail.com>
2020-02-21 02:16:16 +0000

    uploading task #7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+/**
+* cap_string - function that capitalizes all words of a string
+* @a: string
+* Return: string changed
+**/
+
+char *leet(char *a)
+{
+	int i, j;
+	char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char numbers[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (letters[j] == a[i])
+			{
+				a[i] = numbers[j];
+			}
+		}
+	}
+	return (a);
+}
