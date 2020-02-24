d62217fd9aa508dc77719ea0bea6c66f6ea7f1eb
David Corredor <vidmore8@gmail.com>
2020-02-24 18:52:47 +0000

    fix #2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,20 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i = 0, a = 0;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
 		i++;
+	}
+	i++;
+	while (a <= i)
+	{
+		if (s[a] == c)
+		{
+			return (s + a);
+		}
+		a++;
 	}
 	return ('\0');
 }
