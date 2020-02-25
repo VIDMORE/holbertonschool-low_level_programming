fd656410f08eb7fc1d1232231044aa11b5a4e16b
David Corredor <vidmore8@gmail.com>
2020-02-25 01:33:13 +0000

    task #7

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void set_string(char **s, char *to)
+{
+	int = 0;
+
+	for (i = 0; to[i]; i++)
+	{
+		to[] =
+	}
+}
