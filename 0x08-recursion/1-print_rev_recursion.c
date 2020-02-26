818dbbf54384450887492b91317e21cbe1ace495
David Corredor <vidmore8@gmail.com>
2020-02-26 17:18:57 +0000

    Fix in task #1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,14 +9,10 @@ void _print_rev_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] != '\0')
+	if (s[i])
 	{
 		i++;
 		_print_rev_recursion((s + i));
 		_putchar(s[0]);
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
