4a4bba70a06e0f150912721fab921c71cbaf9ed0
David Corredor <vidmore8@gmail.com>
2020-02-26 19:20:02 +0000

    adding #5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,5 @@
+#include "holberton.h"
+
 /**
 * _sqrt_recursion - function that returns the natural square root of a number
 * @n: number
@@ -6,5 +8,26 @@
 
 int _sqrt_recursion(int n)
 {
+	return (div(n, 0));
+}
 
+/**
+* div - function that validates the pow of a number
+* @n: number to examinate
+* @s: number to pow
+* Return: sqrt
+**/
+
+int div(int n, int s)
+{
+	if (n == (s * s))
+	{
+		return (s);
+	}
+	else if ((s * s) >= n)
+	{
+		return (-1);
+	}
+	else
+		return (div(n, s + 1));
 }
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -2,5 +2,7 @@ char _putchar(char c);
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
-int count(char *s, int num);
 int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int div(int n, int s);
