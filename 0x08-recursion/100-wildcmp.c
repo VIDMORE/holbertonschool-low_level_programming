a9dce54284227a7692fd26aee25b3d413d349276
David Corredor <vidmore8@gmail.com>
2020-02-27 00:25:18 +0000

    uploading #100

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+*
+*
+*
+**/
+
+int wildcmp(char *s1, char *s2)
+{
+	int compare(si, s2);
+}
+
+/**
+* compare - compare the two arrays
+* @s1: first array
+* @s2: second array
+* Return: 
+**/
+
+int compare(int *s1, int *s2)
+{
+	if (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return(1);
+		else
+			return (compare((s1 + 1), (s2 + 1)));
+	}
+	else if (*s2 == '*')
+	{
+		return (compare(s1,(s2 + 1)));
+	}
+	else if(*s1 != s2 && *s2 != '*')
+	{
+		return(compare((s1 + 1),s2));
+	}
+}
