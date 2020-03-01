fa17529b3131f830e381abf005582246edd07926
David Corredor <vidmore8@gmail.com>
2020-03-01 02:36:12 +0000

    uploading task #1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into it.
+ * @argc: Number of arguments
+ * @argv: Array of strings.
+ * Return: 0.
+ **/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
