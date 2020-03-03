ec7920bdea27cf11797e73f260827313d40332a7
David Corredor <vidmore8@gmail.com>
2020-03-03 23:58:27 +0000

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,64 +1,66 @@
-
-#include <stdio.h>
+nclude "holberton.h"
 #include <stdlib.h>
+
 int _strlen(char *s);
 
 /**
-* _strlen - function that returns the length of a string.
-* @s: string
-* Return: length of string
-**/
-
+ * _strlen - returns the length of a string.
+ * @s: string input.
+ * Return: length of an string.
+ */
 int _strlen(char *s)
 {
-	int i;
+	int length;
 
-	i = 0;
-	while (s[i] != '\0')
+	length = 0;
+
+	while (s[length] != '\0')
 	{
-		i++;
+		length++;
 	}
-	return (i);
+
+	return (length);
 }
 
 /**
-* str_concat - function that concatenates two strings
-* @s1: String 1
-* @s2: String 2
-* Return: A pointer to the array.
- **/
+ * *str_concat - function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer should point to a newly allocated space in memory which
+ * contains the contents of s1, followed by the contents of s2 ending with null
+ * return NULL on failure
+ */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, size_1, size_2, full_size;
-	char *ar;
+	int i, size_s1, size_s2, total_size;
+	char *new_arr;
 
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-	size_1 = _strlen(s1);
-	size_2 = _strlen(s2);
+	size_s1 = _strlen(s1);
+	size_s2 = _strlen(s2);
+	total_size = size_s1 + size_s2;
 
-	full_size = size_1 + size_2;
-
-	ar = malloc((sizeof(char) * (full_size + 1)));
-	if (ar == NULL)
+	new_arr = malloc((total_size * sizeof(char)) + 1);
+	if (new_arr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < size_1; i++)
+	for (i = 0 ; i < size_s1 ; i++)
 	{
-		ar[i] = s1[i];
+		new_arr[i] = s1[i];
 	}
-	for (i = 0; i < size_2; i++)
+	for (i = 0 ; i < size_s2 ; i++)
 	{
-		ar[size_1 + i] = s2[i];
+		new_arr[size_s1 + i] = s2[i];
 	}
-	ar[full_size + 1] = '\0';
-	return (ar);
+	new_arr[total_size + 1] = '\0';
+	return (new_arr);
 }
