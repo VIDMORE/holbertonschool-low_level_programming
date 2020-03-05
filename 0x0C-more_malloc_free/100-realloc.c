d8ba2e2bdefc18de7ab293147e5fd9e96b4f9f1f
David Corredor <vidmore8@gmail.com>
2020-03-05 23:24:43 +0000

    add 100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/**
+* _memcpy - function that copies memory area
+* @dest: destination value
+* @src: source value
+* @n: number of bytes
+* Return: a pointer to dest
+**/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/**
+* realloc - function that reallocates a memory block using malloc and free
+* @ptr: pointer
+* @old_size: old size
+* @new_size: new size
+**/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *ar;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		ar = malloc(new_size);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return(NULL);
+	}
+	ar = malloc(new_size);
+	
+	_memcpy(ar, ptr, old_size);
+
+	free(ptr);
+	return (ar);
+}
