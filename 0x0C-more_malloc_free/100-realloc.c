e2a10bcfb035d778f624b1b018174da8c6490a04
David Corredor <vidmore8@gmail.com>
2020-03-05 23:42:28 +0000

    fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,14 +36,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
+	{
 		ar = malloc(new_size);
+		if (ar == NULL)
+			return (NULL);
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 	ar = malloc(new_size);
-
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
 	_memcpy(ar, ptr, old_size);
 
 	free(ptr);
