cbeabf91809e463fcdac7f05ce348ded2a511db7
David Corredor <vidmore8@gmail.com>
2020-03-05 18:53:13 +0000

    first push

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*
+*
+*
+*
+**/
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(sizeof(b));
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
