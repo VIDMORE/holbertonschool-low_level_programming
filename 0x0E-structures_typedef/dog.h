2c8336dbf4d659ecff3c263f82979886f0f84162
David Corredor <vidmore8@gmail.com>
2020-03-09 21:06:34 +0000

    uploading new repo

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+* struct dog - a simple guau guau
+* @name: the dogo name
+* @age: my age
+* @owner: my owner
+**/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
