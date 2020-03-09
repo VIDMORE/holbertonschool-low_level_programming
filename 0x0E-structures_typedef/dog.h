28245b827f619344c645d0738cb7e9d84a0235e1
David Corredor <vidmore8@gmail.com>
2020-03-09 22:56:05 +0000

    task #3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG
 #define DOG
+#include <stdio.h>
 
 /**
 * struct dog - a simple guau guau
@@ -8,12 +9,12 @@
 * @owner: my owner
 **/
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
