8cabe5471ba325296dd85d9f99f8a7479b799776
David Corredor <vidmore8@gmail.com>
2020-03-10 00:26:03 +0000

    task #4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,69 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+char *_strcpy(char *dest, char *src);
+
+/**
+* _strcpy - function that copies a string
+* @dest: pointer of destination
+* @src: string to copy
+* Return: the pointer to dest
+**/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
+/**
+* new_dog - function that creates a new dog.
+* @name: new name
+* @age: new age
+* @owner: new owner
+* Return: instance
+**/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0, j = 0;
+	char *name_storage;
+	char *owner_storage;
+	dog_t *new_perrito;
+
+	while (name[i])
+		i++;
+	while (owner[j])
+		j++;
+	name_storage =  malloc(sizeof(i));
+	if (!name_storage)
+		return (NULL);
+	owner_storage = malloc(sizeof(j));
+	if (!owner_storage)
+	{
+		free(name_storage);
+		return (NULL);
+	}
+	new_perrito = malloc(sizeof(dog_t));
+	if (!new_perrito)
+	{
+		free(name_storage);
+		free(owner_storage);
+		return (NULL);
+	}
+	_strcpy(name_storage, name);
+	_strcpy(owner_storage, owner);
+
+	new_perrito->name = name_storage;
+	new_perrito->age = age;
+	new_perrito->owner = owner_storage;
+
+	return (new_perrito);
+}
