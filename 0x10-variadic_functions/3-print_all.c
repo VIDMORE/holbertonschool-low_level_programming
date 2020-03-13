5525ce4cc32c385039a8ae4c32531b010a2e6cc8
David Corredor <vidmore8@gmail.com>
2020-03-13 21:13:38 +0000

    adding full valgrind

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,86 +1,85 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdio.h>
 
 /**
  * print_all - function that prints anything
- *
- *
- *
+ * @format: type of identifiers
  **/
 
 void print_all(const char * const format, ...)
 {
-	int i = 0, j = 0, count = 0;
+	int i = 0, j = 0;
 	va_list args;
-	char *compare;
+	char *c = "";
 	opts opti[] = {
-		{ "c", print_c},
-		{ "i", print_i},
-		{ "f", print_f},
-		{ "s", print_s},
+		{'c', print_c},
+		{'i', print_i},
+		{'f', print_f},
+		{'s', print_s},
 	};
-	
-	while (i < 4)
+	va_start(args, format);
+
+	while (format[i] != '\0' && format != NULL)
 	{
+		j = 0;
 		while (j < 4)
 		{
 			if (opti[j].type == format[i])
-				count ++;
+			{
+				printf("%s", c);
+				opti[j].print_type(args);
+				break;
+			}
 			j++;
 		}
+		c = ", ";
 		i++;
 	}
+	printf("\n");
 }
 
 /**
-* print_c - print charts
-*
-*
-*
-**/
+ * print_c - print charts
+ * @arg: va_list
+ **/
 
-void print_c(va_list)
+void print_c(va_list arg)
 {
-
-va_
-
-
+	printf("%c", va_arg(arg, int));
 }
 
 /**
-* print_i - 
-*
-*
-*
-*
-**/
+ * print_i - print int
+ * @arg: va_list
+ **/
 
-void print_i(va_list)
+void print_i(va_list arg)
 {
-
+	printf("%d", va_arg(arg, int));
 }
 
 /**
-* print_f - print floats
-*
-*
-*
-**/
+ * print_f - print floats
+ * @arg: va_list
+ **/
 
-void print_f(va_list)
+void print_f(va_list arg)
 {
-
-
+	printf("%f", va_arg(arg, double));
 }
 
 /**
-* print_s - print string
-*
-*
-*
-**/
+ * print_s - print string
+ * @arg: va_list
+ **/
 
-void print_s(va_list)
+void print_s(va_list arg)
 {
+	char *s;
 
+	s = va_arg(arg, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,15 +1,16 @@
 #ifndef MAGO
 #define MAGO
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 /**
-*
-*
-*
-*
+* struct options - options for the function
+* @type: type to print
+* @print_type: pointer to function
 **/
 
 typedef struct options
@@ -17,6 +18,11 @@ typedef struct options
 	char type;
 	void (*print_type)(va_list);
 
-}opts
+} opts;
+
+void print_c(va_list arg);
+void print_s(va_list arg);
+void print_f(va_list arg);
+void print_i(va_list arg);
 
 #endif
