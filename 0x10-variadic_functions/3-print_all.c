768a596b6de7bba08a9bc22b2020e5fb41b9700c
David Corredor <vidmore8@gmail.com>
2020-03-15 23:24:58 +0000

    gaminada

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,84 +3,87 @@
 #include <stdio.h>
 
 /**
- * print_all - function that prints anything
- * @format: type of identifiers
- **/
+ * fun_char - Validate char.
+ * @chr: Char argument.
+ */
+void fun_char(va_list chr)
+{
+	printf("%c", va_arg(chr, int));
+}
 
+/**
+ * fun_int - Integer validate.
+ * @integer: Integer argument
+ */
+void fun_int(va_list integer)
+{
+	printf("%i", va_arg(integer, int));
+}
+
+/**
+ * fun_float - Float validation.
+ * @flt: Float arguments.
+ */
+void fun_float(va_list flt)
+{
+	printf("%f", va_arg(flt, double));
+}
+
+/**
+ * fun_str - String validation.
+ * @str: String arguments.
+ */
+void fun_str(va_list str)
+{
+	char *string;
+
+	string = va_arg(str, char *);
+
+	if (string == NULL)
+	{
+		string = "(nil)";
+	}
+	printf("%s", string);
+}
+
+/**
+ * print_all - Function that prints anything.
+ * @format: List of types of arguments passed to the function.
+ * Return: Nothing.
+ */
 void print_all(const char * const format, ...)
 {
-	int i = 0, j = 0;
-	char *c = "";
-	opts opti[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{"s", print_s},
+	va_f form[] = {
+		{"c", fun_char},
+		{"i", fun_int},
+		{"f", fun_float},
+		{"s", fun_str},
 		{NULL, NULL}
 	};
-	va_list args;
 
-	va_start(args, format);
-	while (format[i] && format)
+	int i, j;
+	char *comma = "";
+	va_list list;
+
+	va_start(list, format);
+
+	i = 0;
+	while (format && format[i])
 	{
 		j = 0;
 		while (j < 4)
 		{
-			if (*opti[j].type == format[i])
+			if (format[i] == *form[j].va)
 			{
-				printf("%s", c);
-				opti[j].print_type(args);
-				c = ", ";
+				printf("%s", comma);
+				form[j].f(list);
+				comma = ", ";
 			}
 			j++;
 		}
 		i++;
 	}
-	va_end(args);
+
+	va_end(list);
 	printf("\n");
-}
-
-/**
- * print_c - print charts
- * @arg: va_list
- **/
-
-void print_c(va_list arg)
-{
-	printf("%c", va_arg(arg, int));
-}
-
-/**
- * print_i - print int
- * @arg: va_list
- **/
-
-void print_i(va_list arg)
-{
-	printf("%d", va_arg(arg, int));
-}
-
-/**
- * print_f - print floats
- * @arg: va_list
- **/
-
-void print_f(va_list arg)
-{
-	printf("%f", va_arg(arg, double));
-}
-
-/**
- * print_s - print string
- * @arg: va_list
- **/
-
-void print_s(va_list arg)
-{
-	char *s;
-
-	s = va_arg(arg, char *);
-	if (s == NULL)
-		s = "(nil)";
-	printf("%s", s);
 }
