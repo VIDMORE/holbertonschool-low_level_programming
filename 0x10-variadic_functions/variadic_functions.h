b9cdfe204d4b25dcce4d40882beb014e4f65fb12
David Corredor <vidmore8@gmail.com>
2020-03-15 23:27:32 +0000

    fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,27 +2,21 @@
 #define MAGO
 #include <stdarg.h>
 
+/**
+ * struct va - Variadic global structure.
+ * @va: Struct.
+ * @f: Function.
+ */
+typedef struct va
+{
+	char *va;
+	void (*f)();
+} va_f;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-/**
-* struct options - options for the function
-* @type: type to print
-* @print_type: pointer to function
-**/
-
-typedef struct options
-{
-	char *type;
-	void (*print_type)(va_list);
-
-} opts;
-
-void print_c(va_list arg);
-void print_s(va_list arg);
-void print_f(va_list arg);
-void print_i(va_list arg);
 
 #endif
