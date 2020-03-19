d9e7bd520eb38cedc83d523a9265a5c55057b243
David Corredor <vidmore8@gmail.com>
2020-03-19 18:52:37 +0000

    fix ??

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,6 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
@@ -23,7 +22,7 @@ list_t *add_node(list_t **head, const char *str)
 	new->next = *head;
 	*head = new;
 
-	return (*head);
+	return (new);
 }
 
 /**
