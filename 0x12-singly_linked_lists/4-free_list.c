a2c628f107362768e4798898fd8d4f2759464e09
David Corredor <vidmore8@gmail.com>
2020-03-19 21:16:34 +0000

    free baby

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,8 +1,20 @@
 #include "lists.h"
 
 /**
- *
- *
- *
- *
+ * free_list - Function that frees a list
+ * @head: first element of the list
  */
+void free_list(list_t *head)
+{
+	list_t *temporal, *saver;
+
+	temporal = head;
+
+	while (temporal != NULL)
+	{
+		saver = temporal->next;
+		free(temporal->str);
+		free(temporal);
+		temporal = saver;
+	}
+}
