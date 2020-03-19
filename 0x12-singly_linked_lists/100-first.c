8ea75f88b5997f97a5bd0ea5cf9a17e97b7e22ef
David Corredor <vidmore8@gmail.com>
2020-03-19 21:50:37 +0000

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * i_am_speed - function that prints before the main
+ */
+
+void i_am_speed(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
