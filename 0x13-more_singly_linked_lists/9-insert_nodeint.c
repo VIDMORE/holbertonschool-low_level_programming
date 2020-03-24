0f541ba508dc0b5db7cff21e0eaf33d47902ef0e
David Corredor <vidmore8@gmail.com>
2020-03-24 18:46:03 +0000

    fix in 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,12 +21,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new->n = n;
 
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	while (contador)
 	{
 		contador = contador->next;
 		counter++;
 	}
-
 	if (idx == counter)
 	{
 		new->next = NULL;
