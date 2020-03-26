704bae850b6d64ca836b2a4727a3f8ca1f2f1a31
David Corredor <vidmore8@gmail.com>
2020-03-26 22:33:43 +0000

    uploading task #1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-void reverse_print(unsigned long int n);
 
 /**
  * print_binary - prints the binary representation of a number
@@ -8,28 +7,10 @@ void reverse_print(unsigned long int n);
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
+
+	if (n > 1)
 	{
-		_putchar('0');
-		return;
+		print_binary(n >> 1);
 	}
-
-	reverse_print(n);
-}
-
-/**
- * reverse_print - Function that prints a binary in reverse
- * @n: number
- */
-
-void reverse_print(unsigned long int n)
-{
-	if (n == 0)
-	{
-		return;
-	}
-	else
-		reverse_print(n >> 1);
-
 	_putchar((n & 1) + '0');
 }
