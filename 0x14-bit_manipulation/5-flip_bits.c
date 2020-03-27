66d9924566f576961ba18bf4d0cecf0bb98a15d9
David Corredor <vidmore8@gmail.com>
2020-03-27 01:53:04 +0000

    uploading task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - count the number of bits to be fliped
+ * @n: first number
+ * @m: second number
+ * Return: number of flips
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{	
+	unsigned int counter = 0;
+	unsigned long int num;
+
+	num = (n ^ m);
+
+	while (num != 0)
+	{
+		if (num & 1)
+			counter++;
+		num >>= 1;
+	}
+
+	return (counter);
+}
