a1ab15c629b0915916fbd06dbd5464ca5845c7e3
David Corredor <vidmore8@gmail.com>
2020-03-31 16:17:00 -0500

    Uploading task #1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,9 +18,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-
-	while (text_content[i] != '\0')
-		i++;
+		
+	if (text_content)
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
 
 	wr = write(fd, text_content, i);
 
