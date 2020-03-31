92f6aafd5fb3389ec71d5897f6a701a5cee511cf
David Corredor <vidmore8@gmail.com>
2020-03-31 16:09:33 -0500

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, i;
+	int fd, wr, i = 0;
 
 	if (!filename)
 		return (-1);
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	
+
 	while (text_content[i] != '\0')
 		i++;
 
