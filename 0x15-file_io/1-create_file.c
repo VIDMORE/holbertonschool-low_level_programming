cded917428033b3d3d9c2f97a15b85c546cd3b6f
David Corredor <vidmore8@gmail.com>
2020-03-31 16:07:15 -0500

    uploaidng task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file
+ * @text_content: text
+ * Return: write
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr, i;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_EXCL | O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+	
+	while (text_content[i] != '\0')
+		i++;
+
+	wr = write(fd, text_content, i);
+
+	if (wr == -1)
+		return (-1);
+
+	return (1);
+}
