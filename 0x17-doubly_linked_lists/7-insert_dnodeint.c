cd140bc9f9d3130f10e2a8b60e150e05b042299c
David Corredor <vidmore8@gmail.com>
2020-05-02 09:03:33 -0500

    [Fix] - Validation if h == null

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,10 +15,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (count->next)
 		count = count->next, counter++;
-
 	if (idx > counter)
 		return (NULL);
-
+	if (!*h)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
