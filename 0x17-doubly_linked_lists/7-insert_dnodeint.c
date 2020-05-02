59fa4fdb4ccc7b4a3515afc19d8fc4d2c0aa5906
David Corredor <vidmore8@gmail.com>
2020-05-02 16:23:57 -0500

    [Fix] ins task #7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->next = NULL;
 	new->prev = NULL;
 
-	while (i < (idx - 1))
+	while (temporal && i < (idx - 1))
 	{
 		temporal = temporal->next;
 		i++;
