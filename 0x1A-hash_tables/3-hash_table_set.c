8ecc17b174ee18d31fb1f84148dc994524f7ef2e
David Corredor <vidmore8@gmail.com>
2020-05-29 10:37:03 -0500

    [FIX] - Task #3 and #4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *node = ht->array[index], *tmp = NULL, *spot;
 
-	if (!key || !value || strlen(key) < 0)
+	if (!key || !value || strlen(key) <= 0)
 		return (0);
 
 	if (node == 0)
