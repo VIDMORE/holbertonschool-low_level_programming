cbc9631851b2467ad22c68af8bbf31464447040f
David Corredor <vidmore8@gmail.com>
2020-08-26 13:33:18 -0500

    [UPLOAD] - Task 104

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
-* binary_search - Binary search algorithm
+* advanced_binary - Binary search algorithm
 *
 * @array: Pointer to the beginning of an array
 * @size: Size of the array
